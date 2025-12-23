6c3ef24c2be6c5df38abf627a4a173948882cf47
rgcastrof <rogeriogirao1@proton.me>
2025-12-23 20:54:08 -0300

    integrate photo list from database into viewmodel and uistate

--- TrustCam/app/src/main/java/com/rgcastrof/trustcam/uistate/CameraUiState.kt
+++ TrustCam/app/src/main/java/com/rgcastrof/trustcam/uistate/CameraUiState.kt
@@ -1,7 +1,9 @@
 package com.rgcastrof.trustcam.uistate
 
 import androidx.camera.core.CameraSelector
+import com.rgcastrof.trustcam.data.model.Photo
 
 data class CameraUiState(
-    val cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    val cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA,
+    val photos: List<Photo> = emptyList()
 )
