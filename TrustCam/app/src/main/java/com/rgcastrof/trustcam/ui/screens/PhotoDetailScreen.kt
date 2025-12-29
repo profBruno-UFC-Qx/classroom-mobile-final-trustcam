4f7cbb93feb22c820eade8f6500073a95105a38e
rgcastrof <rogeriogirao1@proton.me>
2025-12-29 11:30:54 -0300

    implement back and delete buttons on photo screen

--- TrustCam/app/src/main/java/com/rgcastrof/trustcam/ui/screens/PhotoDetailScreen.kt
+++ TrustCam/app/src/main/java/com/rgcastrof/trustcam/ui/screens/PhotoDetailScreen.kt
@@ -1,40 +1,78 @@
 package com.rgcastrof.trustcam.ui.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.DeleteOutline
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.rgcastrof.trustcam.data.model.Photo
 
 @Composable
 fun PhotoDetailScreen(
-    photo: Photo?,
-    onBackClick: () -> Unit
+    photo: Photo,
+    onBackClick: () -> Unit,
+    onDeleteClick: (photo: Photo) -> Unit,
 ) {
     Box(
         modifier = Modifier.safeDrawingPadding()
     ) {
         AsyncImage(
-            model = photo?.filePath,
+            model = photo.filePath,
             contentDescription = null,
             modifier = Modifier.align(Alignment.Center).fillMaxSize(),
             contentScale = ContentScale.Fit
         )
+        Row(modifier = Modifier.align(Alignment.TopStart)) {
+            IconButton(
+                onClick = onBackClick,
+            ) {
+                Icon(Icons.AutoMirrored.Default.ArrowBack, contentDescription = null)
+            }
+            Text(
+                text = "date",
+            )
+        }
         IconButton(
-            onClick = onBackClick,
-            modifier = Modifier.align(Alignment.TopStart).padding(16.dp)
+            onClick = {},
+            modifier = Modifier.align(Alignment.TopEnd),
         ) {
-            Icon(Icons.AutoMirrored.Default.ArrowBack, contentDescription = null)
+            Icon(
+                Icons.Default.MoreVert,
+                contentDescription = null
+            )
+        }
+        Row(modifier = Modifier.align(Alignment.BottomStart)) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .clickable(onClick = {
+                            onDeleteClick(photo)
+                            onBackClick()
+                        })
+                        .padding(10.dp)
+                ) {
+                    Icon(Icons.Default.DeleteOutline, contentDescription = null)
+                    Text(
+                        text = "Delete",
+                        fontSize = 12.sp,
+                    )
+                }
         }
     }
 }
