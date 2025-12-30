4bc9e118590c127a9e78d6254a973068045ed266
rgcastrof <rogeriogirao1@proton.me>
2025-12-30 12:13:45 -0300

    separate viewmodels and refactor photo detail logic

--- TrustCam/app/src/main/java/com/rgcastrof/trustcam/data/TrustCamApplication.kt
+++ TrustCam/app/src/main/java/com/rgcastrof/trustcam/data/TrustCamApplication.kt
@@ -3,7 +3,7 @@ package com.rgcastrof.trustcam.data
 import android.app.Application
 import com.rgcastrof.trustcam.data.repository.CameraRepository
 
-class CameraApplication : Application() {
+class TrustCamApplication : Application() {
     val database by lazy { CameraDatabase.getInstance(this) }
     val repository by lazy { CameraRepository(database.photoDao()) }
 }
